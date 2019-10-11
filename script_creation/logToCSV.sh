cb33e1dca0fd4ebe4c98289026077983d5251670
Gian Marco Forghieri <G.M.Forghieri@student.tudelft.nl>
2019-10-11 20:46:34 +0200

    Initial commit

---
+++ script_creation/logToCSV.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This program should take an fileIn as first parameter
+# It takes the input log file that has the same format as access_log and maps it to a csv format
+# the csv format is:
+# Client,Time,Type,Path,Status,Size
+#
+# The program should not create a csv file.
+# This can be done by piping the output to a file.
+# example: './logToCSV access_log > output.csv'
+# It could take some time to convert all of access_log. Consider using a small subset for testing.
